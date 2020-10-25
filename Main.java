ecfe198cefc9016023af58c8228e902f6209364a
Seventhy-Seven <72259094+Seventhy-Seven@users.noreply.github.com>
2020-10-25 18:06:23 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+class three{
+    private int one;
+    private int two;
+
+    three(){
+        System.out.println();
+    }
+
+    three(int one){
+        System.out.println();
+        this.one = one;
+    }
+
+    three(int one, int two){
+        this.one = one;
+        this.two = two;
+    }
+
+    void getprint(){
+        System.out.println("one = " + one);
+        System.out.println("two = " + two);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        three tri = new three();
+        tri.getprint();
+
+        tri = new three(2);
+        tri.getprint();
+
+        tri = new three(3,4);
+        tri.getprint();
+    }
+}
